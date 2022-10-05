52c9d5ff120ea304af5808f4c178eb1688df5569
vanja-veapi <vanja.veapi@gmail.com>
2022-10-05 19:29:40 +0200

    Type Aliases

--- typescript-tutorial/src/sandbox.ts
+++ typescript-tutorial/src/sandbox.ts
@@ -8,3 +8,7 @@ const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => {
 	console.log(input);
 });
+
+//Type Aliases
+type StringOrNum = string | number;
+const id: StringOrNum = 1;
