3f6ebb29635194a853727ee6cec157d60a2c21eb
vanja-veapi <vanja.veapi@gmail.com>
2022-10-05 11:55:03 +0200

    TypeScript Workflow

---
+++ typescript-tutorial/public/sandbox.js
@@ -0,0 +1,8 @@
+"use strict";
+// ? Workflow testing...
+const character = "mario";
+console.log(character);
+const inputs = document.querySelectorAll("input");
+inputs.forEach((input) => {
+    console.log(input);
+});
---
+++ typescript-tutorial/src/sandbox.ts
@@ -0,0 +1,10 @@
+// ? Workflow testing...
+const character = "mario";
+
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+	console.log(input);
+});
